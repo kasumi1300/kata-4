af7832a35e9419fc22eb9de0cdbf4b43796cb0a2
Rake <Rake@192.168.1.40>
2013-10-17 21:51:49 +0100

    First commit

---
+++ NetBeansProjects/personal/src/personal/Personal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personal;
+
+/**
+ *
+ * @author Rake
+ */
+public class Personal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
